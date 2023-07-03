00317ba64aa3709c024f3da4df75598d7f92eca9
SergBily <sergbily.dev@gmail.com>
2023-07-03 16:40:19 +0300

    fix: add default port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { createServer } from 'http';
 import { router } from './router/router';
 
 dotenv.config();
-const port = process.env.PORT;
+const DEFAULT_PORT = 4000;
+const port = process.env.PORT || DEFAULT_PORT;
 const server = createServer(router);
 
 server.listen(port, () => {
